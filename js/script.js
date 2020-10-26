55c99a768ae35233a7d5dba7639f9599cdcc3be5
griffinbatson <72712366+griffinbatson@users.noreply.github.com>
2020-10-26 14:32:58 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+d3.selectAll(".bus1")
+    .on("click",function(){
+
+    d3.select(".bus1")
+     .classed("hidden", true)
+        })
+d3.selectAll(".bus2")
+    .on("click",function(){
+
+    d3.select(".bus2")
+     .classed("hidden", true) 
+        })
+d3.selectAll(".bus3")
+    .on("click",function(){
+
+    d3.select(".bus3")
+     .classed("hidden", true)
+        })
+d3.selectAll(".bus4")
+    .on("click",function(){
+
+    d3.select(".bus4")
+     .classed("hidden", true) 
+        })
+d3.selectAll(".nobus1")
+    .on("click",function(){
+
+    d3.select(".nobus1")
+     .classed("hidden", true)
+        })
+d3.selectAll(".nobus2")
+    .on("click",function(){
+
+    d3.select(".nobus2")
+     .classed("hidden", true) 
+        })
+d3.selectAll(".nobus3")
+    .on("click",function(){
+
+    d3.select(".nobus3")
+     .classed("hidden", true) 
+        })
+d3.selectAll(".nobus4")
+    .on("click",function(){
+
+    d3.select(".nobus4")
+     .classed("hidden", true) 
+        })
+d3.selectAll(".nobus5")
+    .on("click",function(){
+
+    d3.select(".nobus5")
+     .classed("hidden", true) 
+        })
+d3.selectAll(".reset")
+    .on("click",function(){
+    
+    d3.selectAll("img")
+    .classed("hidden", false)
+})
